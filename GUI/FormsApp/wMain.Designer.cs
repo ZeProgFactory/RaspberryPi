3b40637eb6ee174af4cb89bc149edc314d335a81
Michael <zeprogramfactory@outlook.com>
2021-02-09 11:57:56 +0100

    xxx

---
+++ GUI/FormsApp/wMain.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace FormsApp
+{
+    partial class wMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBody = new System.Windows.Forms.Panel();
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panelButton = new System.Windows.Forms.Panel();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.panelTitle.SuspendLayout();
+            this.panelButton.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelBody
+            // 
+            this.panelBody.BackColor = System.Drawing.SystemColors.Info;
+            this.panelBody.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBody.Location = new System.Drawing.Point(10, 10);
+            this.panelBody.Margin = new System.Windows.Forms.Padding(10);
+            this.panelBody.Name = "panelBody";
+            this.panelBody.Size = new System.Drawing.Size(1542, 1068);
+            this.panelBody.TabIndex = 0;
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panelTitle.Controls.Add(this.labelTitle);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(10, 10);
+            this.panelTitle.Margin = new System.Windows.Forms.Padding(10);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(1542, 173);
+            this.panelTitle.TabIndex = 1;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(0, 0);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(1542, 173);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "FormsApp";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelButton
+            // 
+            this.panelButton.Controls.Add(this.btnInfo);
+            this.panelButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelButton.Location = new System.Drawing.Point(1275, 183);
+            this.panelButton.Margin = new System.Windows.Forms.Padding(10);
+            this.panelButton.Name = "panelButton";
+            this.panelButton.Padding = new System.Windows.Forms.Padding(10);
+            this.panelButton.Size = new System.Drawing.Size(277, 895);
+            this.panelButton.TabIndex = 2;
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInfo.Location = new System.Drawing.Point(10, 10);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(257, 640);
+            this.btnInfo.TabIndex = 0;
+            this.btnInfo.Text = "info";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            // 
+            // wMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1562, 1088);
+            this.Controls.Add(this.panelButton);
+            this.Controls.Add(this.panelTitle);
+            this.Controls.Add(this.panelBody);
+            this.Name = "wMain";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "FormsApp";
+            this.panelTitle.ResumeLayout(false);
+            this.panelButton.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBody;
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Panel panelButton;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button btnInfo;
+    }
+}
+
