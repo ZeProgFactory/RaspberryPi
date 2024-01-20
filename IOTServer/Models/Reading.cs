389110e544b2fb531c92dc4556da6b9189cd4367
Michael <Michael@ZPF.fr>
2024-01-20 07:09:22 +0100

    IOTServer: Reading: Unique index

--- IOTServer/Models/Reading.cs
+++ IOTServer/Models/Reading.cs
@@ -3,10 +3,10 @@ using ZPF.SQL;
 
 public class Reading
 {
-    public enum Origines { mesure, calculated }
+   public enum Origines { mesure, calculated }
 
-    [ZPF.SQL.DB_Attributes.Index(false, SortDirections.desc)]
-    public DateTime TimeStamp { get; set; } = DateTime.Now;
-    public decimal Temp { get; set; }
-    public Origines Origine { get; set; } = Origines.mesure;
+   [ZPF.SQL.DB_Attributes.Index(true, SortDirections.desc)]
+   public DateTime TimeStamp { get; set; } = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, 0, 0);
+   public decimal Temp { get; set; }
+   public Origines Origine { get; set; } = Origines.mesure;
 }
